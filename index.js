9de5395cfe17d2ae007e6990994aaef1da3d5d49
nickolanack <nickblackwell82@gmail.com>
2017-03-28 23:20:47 -0700

    initial commit websocket server listens on port 8080. and allows mouse movement and click from remote client

---
+++ index.js
@@ -0,0 +1,52 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({
+	//perMessageDeflate: false,
+	port: 8080
+}, function onListening() {
+
+	console.log('listening');
+	console.log(arguments);
+
+});
+
+
+const robot = require("robotjs");
+var mouse = robot.getMousePos();
+var last=null;
+
+
+wss.on('connection', function connection(ws) {
+	console.log('connection');
+	mouse = robot.getMousePos();
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+
+
+
+		if(message.indexOf('{')===0){
+			var gesture=JSON.parse(message);
+
+			if(last!==gesture.event){
+				console.log('store mouse on: '+gesture.event);
+				mouse = robot.getMousePos();
+				last=gesture.event;
+			}
+
+			
+			if(gesture.event==='move'){
+				robot.moveMouse(mouse.x+gesture.dx, mouse.y+gesture.dy);
+			}
+
+			if(gesture.event==='click'){
+				robot.mouseClick();
+			}
+
+		}
+
+		ws.send(message);
+	});
+
+
+
+});
